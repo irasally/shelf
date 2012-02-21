2ccd78318eaadb61b185fe96608f097475c95e53
irasally <sally0721+github@gmail.com>
2012-02-21 15:39:40 +0900

    9.6-1 added edit action.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,10 @@ class BooksController < ApplicationController
     @book = Book.new
     @years = Array.new((1985..Time.now.year).map{|y| [y,y]}.reverse)
   end
+  def edit
+    @book = Book.find(params[:id])
+    @years = Array.new((1985..Time.now.year).map{|y| [y,y]}.reverse)
+  end
   def create
     @book = Book.create(params[:book])
     redirect_to @book
