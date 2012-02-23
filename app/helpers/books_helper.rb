7153ff1a6e6054e57b5612812f85f1d08eb506e7
irasally <sally0721+github@gmail.com>
2012-02-23 10:32:17 +0900

    13.4-2 added helper method.      link_for: create links for booklist.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,14 @@
+# -*- coding: utf-8 -*-
 module BooksHelper
+  def link_for(book)
+    links = []
+    links << link_to('修正', [:edit, book])
+    if book.checked_out?
+      links << link_to('在庫', [:check_in, book], :method => :put) 
+    else
+      links << link_to('貸出', [:check_out, book], :method => :put) 
+    end
+    links << delete_link(book)
+    raw(links.join(' '))
+  end
 end
