089a23a02443c86eb2774dc33d6acb6026ff2af4
irasally <sally0721+github@gmail.com>
2012-02-24 14:39:41 +0900

    4.5-2 added category model.  - Categoryモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120204080245) do
+ActiveRecord::Schema.define(:version => 20120224052300) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(:version => 20120204080245) do
     t.boolean  "checked_out"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.string   "color"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
