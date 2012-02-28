6b5afd1ed98b141d03a7cfd3823507a6854f1ad9
irasally <sally0721@gmail.com>
2012-02-28 17:26:29 +0900

    6.5-2 add relation category and books.  - BookモデルとCategoryモデルを関連づける。モデルの変更とマイグレーションファイルの作成。

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   validates :name, :presence => true
   validates :color, :presence => true
+  has_many :books
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120224052300) do
+ActiveRecord::Schema.define(:version => 20120228082456) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120224052300) do
     t.boolean  "checked_out"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "category_id"
   end
 
   create_table "categories", :force => true do |t|
